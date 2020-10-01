441d7b9a3d28824669fa5bcf11075177f5655c90
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:20:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
-array = []
+katz_deli = []
 
-def line(array)
+def line(katz_deli)
   line_positions = []
-  if array.empty?
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     number = 1
-    array.each do |customer|
+    katz_deli.each do |customer|
       line_positions << "#{number}. #{customer}"
       number += 1
       puts "The line is currently: #{line_positions.join(" ")}."
