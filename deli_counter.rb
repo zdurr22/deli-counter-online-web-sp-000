f86f78fa4a3fb4556771de442e66f1e976258f30
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:23:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,4 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome #{name}, you are number #{number} in line"
+  puts "Welcome #{name}, you are number #{katz_deli.size} in line"
