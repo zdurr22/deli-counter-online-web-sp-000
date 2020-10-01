983ac942522fee4a5b8817d5b84e46ce2a7e9795
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:28:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
