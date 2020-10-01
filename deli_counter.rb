9d609e482471ff442c123a1d8db3c3358b51c3d1
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:26:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.empty?
     puts "The line is empty."
   else
